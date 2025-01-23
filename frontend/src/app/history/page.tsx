59bdf274639f048914eb28b390ecc5ad4f02c68a
sei33 <suzuki.sona.23@shizuoka.ac.jp>
2025-01-24 08:28:49 +0900

    fix: history画面サイズが小さいときのデザインを修正

--- frontend/src/app/history/page.tsx
+++ frontend/src/app/history/page.tsx
@@ -5,37 +5,21 @@ import Footer from '../components/layout/footer/page';
 import styles from './history.module.css';
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/voteResult');
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log('取得したデータ:', data);
-      } catch (error) {
-        console.error('データの取得に失敗しました:', error);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
     <div className={styles.carousel}>
       <Header className={styles.carouselItem}></Header>
       <section className={`${styles.carouselItem} ${styles.content}`}>
 
         <main className={styles.main}>
-          <h1 className={styles.title}>ナナちゃんの歴史</h1>
+          <h1 className={styles.title}>これまでの変遷、誕生秘話</h1>
 
           <div className={styles.timeline}>
             <div className={styles.timelineItem}>
               <div className={styles.timelineYear}>1972</div>
               <div className={styles.timelineContent}>
                 <h2>名鉄百貨店セブン館オープン</h2>
-                <p>平成18年「ヤング館」に改称される</p>
-                <p>ここにナナちゃんが来ることになる</p>
+                <p>・2006年「ヤング館」に改称される</p>
+                <p>・ここにナナちゃんが来ることになる</p>
               </div>
             </div>
 
@@ -43,8 +27,10 @@ export default function Home() {
               <div className={styles.timelineYear}>1973</div>
               <div className={styles.timelineContent}>
                 <h2>ナナちゃん誕生</h2>
-                <p>一周年記念の際にシンボルになるものを作ろうと考案</p>
-                <p>巨大マネキン『ナナちゃん』が誕生</p>
+                <p>・名鉄百貨店の一周年記念の際に、シンボルになるものを作ろうと、考案された</p>
+                <p>・百貨店と切っても切れないマネキンを、巨大化させてマスコットにすることに決定</p>
+                <p>・名前は一般公募し、 セブンをもじった親しみやすい名前「ナナちゃん」に決定</p>
+                <p>・こうして誕生して以来、名古屋駅前の顔として長年愛され、待ち合わせの場所などに利用されている。</p>
               </div>
             </div>
 
@@ -52,8 +38,8 @@ export default function Home() {
               <div className={styles.timelineYear}>2011</div>
               <div className={styles.timelineContent}>
                 <h2>名鉄百貨店セブン館閉館</h2>
-                <p>開業から39年たってセブン館は閉館</p>
-                <p>ナナちゃんは広報部員として活動を続ける</p>
+                <p>・開業から39年たってセブン館は閉館</p>
+                <p>・ナナちゃんはその後も名鉄百貨店の広報部員として活動を続ける</p>
               </div>
             </div>
 
@@ -61,8 +47,7 @@ export default function Home() {
               <div className={styles.timelineYear}>2012</div>
               <div className={styles.timelineContent}>
                 <h2>一般企業への広告媒体としての使用解禁</h2>
-                <p>それ以前は公共性のある利用に限られていたが</p>
-                <p>一般企業の利用も可能になった</p>
+                <p>・それ以前は名鉄百貨店関連または公共性のある利用に限られていたが、一般企業の利用も可能になった</p>
               </div>
             </div>
           </div>
