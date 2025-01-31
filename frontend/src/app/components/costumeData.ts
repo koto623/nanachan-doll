a8a13dbd4e2676a74857f19d0516e89c5e4a5dc2
koto623 <akan.02toko@gmail.com>
2025-01-31 14:48:55 +0900

    add: 衣装データを作成

---
+++ frontend/src/app/components/costumeData.ts
@@ -0,0 +1,33 @@
+interface Costume {
+  id: number;
+  title: string;
+  img: string;
+}
+
+export const costumeData = [
+  {
+    id: 0,
+    title: 'costume0',
+    img: 'images/costume0.png',
+  },
+  {
+    id: 1,
+    title: 'costume1',
+    img: 'images/costume1.png',
+  },
+  {
+    id: 2,
+    title: 'costume2',
+    img: 'images/costume2.png',
+  },
+  {
+    id: 3,
+    title: 'costume3',
+    img: 'images/costume3.png',
+  },
+  {
+    id: 4,
+    title: 'costume4',
+    img: 'images/costume4.png',
+  },
+] as Costume[];
