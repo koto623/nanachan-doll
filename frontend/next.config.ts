3e1c09383c6a59a1c1a97976648af31fe817f362
koto623 <akan.02toko@gmail.com>
2025-01-31 14:49:16 +0900

    fix: docker周りを修正

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import { config } from 'process';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
