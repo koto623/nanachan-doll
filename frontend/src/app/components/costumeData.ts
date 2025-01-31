cbc9a01bcc2c724debd9aaf91bcaba81d7b164ac
sei33 <suzuki.sona.23@shizuoka.ac.jp>
2025-01-31 15:53:55 +0900

    add: コスチュームのデータ追加

--- frontend/src/app/components/costumeData.ts
+++ frontend/src/app/components/costumeData.ts
@@ -1,33 +1,189 @@
 interface Costume {
   id: number;
   title: string;
+  date: string;
   img: string;
 }
 
 export const costumeData = [
   {
     id: 0,
-    title: 'costume0',
-    img: 'images/costume0.png',
+    title: 'ひとやすみナナちゃん',
+    date: '1/10~,2/28~',
+    img: 'images/costume01.png',
   },
   {
     id: 1,
-    title: 'costume1',
-    img: 'images/costume1.png',
+    title: '電子申告PRナナちゃん',
+    date: '2/14~',
+    img: 'images/costume02.png',
   },
   {
     id: 2,
-    title: 'costume2',
-    img: 'images/costume2.png',
+    title: 'マイナビナナちゃん',
+    date: '2/21~',
+    img: 'images/costume03.png',
   },
   {
     id: 3,
-    title: 'costume3',
-    img: 'images/costume3.png',
+    title: '名古屋ウィメンズマラソン2024ランナーナナちゃん',
+    date: '3/6~',
+    img: 'images/costume05.png',
   },
   {
     id: 4,
-    title: 'costume4',
-    img: 'images/costume4.png',
+    title: 'ひとやすみナナちゃん',
+    date: '3/11~,5/29~',
+    img: 'images/costume06.png',
+  },
+  {
+    id: 5,
+    title: '祝入学！新入生応援「愛大ナナちゃん」',
+    date: '4/3~',
+    img: 'images/costume07.png',
+  },
+  {
+    id: 6,
+    title: 'ユミコアナナちゃん',
+    date: '4/10~',
+    img: 'images/costume08.png',
+  },
+  {
+    id: 7,
+    title: '母の日はタツノコプロとコラボ！ハッチナナちゃん',
+    date: '4/17~',
+    img: 'images/costume09.png',
+  },
+  {
+    id: 8,
+    title: '4月28日は51回目のバースデー！バースデーナナちゃん',
+    date: '4/28~',
+    img: 'images/costume10.png',
+  },
+  {
+    id: 9,
+    title: 'レントゲンナナちゃん',
+    date: '5/15~',
+    img: 'images/costume11.png',
+  },
+  {
+    id: 10,
+    title: 'JICA海外協力隊ナナちゃん',
+    date: '6/5~',
+    img: 'images/costume13.png',
+  },
+  {
+    id: 11,
+    title: '「三菱の金」ゴールドナナちゃん',
+    date: '6/12~',
+    img: 'images/costume14.png',
+  },
+  {
+    id: 12,
+    title: '「好きなものはよくばっちゃう！」ダブルアイスナナちゃん',
+    date: '6/26~',
+    img: 'images/costume15.png',
+  },
+  {
+    id: 13,
+    title: '映画『キングダム 大将軍の帰還』7月12日(金)公開記念王騎将軍ナナちゃん',
+    date: '7/10~',
+    img: 'images/costume16.png',
+  },
+  {
+    id: 14,
+    title: 'ひとやすみナナちゃん',
+    date: '7/17~,8/28~,9/18~',
+    img: 'images/costume17.png',
+  },
+  {
+    id: 15,
+    title: 'BABY YELL ナナちゃん',
+    date: '8/7~',
+    img: 'images/costume18.png',
+  },
+  {
+    id: 16,
+    title: 'WAO!ナナちゃん',
+    date: '8/14~',
+    img: 'images/costume19.png',
+  },
+  {
+    id: 17,
+    title: '「まめきちまめこ」ナナちゃん',
+    date: '8/21~',
+    img: 'images/costume20.png',
+  },
+  {
+    id: 18,
+    title: '熊撤ナナちゃん',
+    date: '9/11~',
+    img: 'images/costume22.png',
+  },
+  {
+    id: 19,
+    title: '金鯱城ナナちゃん',
+    date: '9/25~',
+    img: 'images/costume24.png',
+  },
+  {
+    id: 20,
+    title: '大阪・関西万博 ナナちゃん',
+    date: '10/2~',
+    img: 'images/costume25.png',
+  },
+  {
+    id: 21,
+    title: '薬剤師ナナちゃん',
+    date: '10/16~',
+    img: 'images/costume26.png',
+  },
+  {
+    id: 22,
+    title: '不法電波をやっつけろ！デンパ君ナナちゃん',
+    date: '10/23~',
+    img: 'images/costume27.png',
+  },
+  {
+    id: 23,
+    title: 'すぱろーナナちゃん',
+    date: '10/30~',
+    img: 'images/costume28.png',
+  },
+  {
+    id: 24,
+    title: '税を考える週間ナナちゃん',
+    date: '11/6~',
+    img: 'images/costume29.png',
+  },
+  {
+    id: 25,
+    title: '果樹産地・長野市PRナナちゃん',
+    date: '11/13~',
+    img: 'images/costume30.png',
+  },
+  {
+    id: 26,
+    title: 'STATION Ai ナナちゃん',
+    date: '11/20~',
+    img: 'images/costume31.png',
+  },
+  {
+    id: 27,
+    title: '祝！名鉄百貨店70周年×ドリカム35周年オリジナルパーカーナナちゃん',
+    date: '11/27~',
+    img: 'images/costume32.png',
+  },
+  {
+    id: 28,
+    title: 'メイエキ×ワオ！イルミ～Don\'t think,feelナナちゃん～',
+    date: '12/11~',
+    img: 'images/costume33.png',
+  },
+  {
+    id: 29,
+    title: '獅子舞ナナちゃん',
+    date: '12/26~',
+    img: 'images/costume34.png',
   },
 ] as Costume[];
